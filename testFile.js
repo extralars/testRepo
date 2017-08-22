981f91de11c7e585671eba767e9ab5ec4579c24a
LW <lw222ii@student.lnu.se>
2017-08-22 09:20:10 +0200

    testin committin

--- testFile.js
+++ testFile.js
@@ -1,1 +1,12 @@
 // just for testing
+// doing an update
+
+// another
+
+function lars() {
+  console.log('lars');
+}
+
+// keep editin
+// i keep on keepin on to keep on n' never stop never stoppin
+
